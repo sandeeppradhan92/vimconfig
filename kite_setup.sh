d64c821602eaf93c93defcdc23ce8201f55ab419
Sandeep Kumar Pradhan <sandeeppradhan92@gmail.com>
2020-05-09 19:09:23 +0530

    Fixed cleanup command error in script
    rm: cannot remove 'kite_installation': No such file or directory

--- kite_setup.sh
+++ kite_setup.sh
@@ -10,4 +10,5 @@ cd kite_installation
 wget -O kite-installer-dropbox https://www.dropbox.com/s/lcdheyto0hqln77/kite-installer.sh?dl=0
 bash kite-installer-dropbox --download
 bash kite-installer-dropbox --install
+cd ..
 rm -r kite_installation
