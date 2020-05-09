928d2d9ee28bd502b55473b164c40a590bda3ff5
Sandeep Kumar Pradhan <sandeeppradhan92@gmail.com>
2020-05-09 19:05:13 +0530

    Create kite_setup.sh
    Created shell script to install kite server and setup the kite plugin for vim. This script is tested in ubuntu server.

---
+++ kite_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get kite plugin for vim
+mkdir -p ~/.vim/pack/kite/start/kite
+git clone https://github.com/kiteco/vim-plugin.git ~/.vim/pack/kite/start/kite/
+
+# instll kite server in the system
+mkdir kite_installation
+cd kite_installation
+wget -O kite-installer-dropbox https://www.dropbox.com/s/lcdheyto0hqln77/kite-installer.sh?dl=0
+bash kite-installer-dropbox --download
+bash kite-installer-dropbox --install
+rm -r kite_installation
